682dc935fa327cc4fbc189742c66b9e4b6a0aba1
Andrés <andresramosarinaga216@gmail.com>
2026-03-05 16:33:31 +0000

    Añado la clase Guerrero con sus atributos propios

---
+++ src/model/Guerrero.java
@@ -0,0 +1,11 @@
+java
+package model;
+
+import model.interfaces.Defendible;
+
+public class Guerrero extends PersonajeFisico implements Defendible {
+
+    private int fuerza;
+    private int armadura;
+
+}
