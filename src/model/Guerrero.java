c485b15139330064bb330221efa7ffc36fad786c
Andrés <andresramosarinaga216@gmail.com>
2026-03-05 16:36:00 +0000

    Añado el metodo sobrescrito para que este personaje tenga su propia forma de atacar

--- src/model/Guerrero.java
+++ src/model/Guerrero.java
@@ -13,4 +13,12 @@ public class Guerrero extends PersonajeFisico implements Defendible {
         this.fuerza = fuerza;
         this.armadura = armadura;
     }
+
+       @Override
+    public void atacar(Personaje objetivo) {
+        int daño = fuerza * 2;
+        objetivo.setSalud(objetivo.getSalud() - daño);
+        System.out.println(nombre + " ataca con espada a " + objetivo.getNombre() + " causando " + daño);
+    }
+
 }
