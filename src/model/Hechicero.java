6fae4605c8d19b2e25da5765b0dd4014b5ac280d
Andrés <andresramosarinaga216@gmail.com>
2026-03-05 16:43:00 +0000

    Añado el constructor para la clase Hechicero son sus valores iniciales

--- src/model/Hechicero.java
+++ src/model/Hechicero.java
@@ -6,5 +6,12 @@ public class Hechicero extends PersonajeMagico {
     private int oscuridad;
     private int invocacion;
 
+    public Hechicero(String nombre, int nivel, int salud, int mana, int oscuridad, int invocacion) {
+        super(nombre, nivel, salud, mana);
+        this.oscuridad = oscuridad;
+        this.invocacion = invocacion;
+    }
+
 }
+
 
