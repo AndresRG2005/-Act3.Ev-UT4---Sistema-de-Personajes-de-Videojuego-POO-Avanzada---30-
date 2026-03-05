d737cefd569af6b930159cd487a16963b76899b1
Andres <andresramosarinaga216@gmail.com>
2026-03-05 23:55:02 +0000

    Añado la clase GestorJuego con el atributo

---
+++ src/controller/GestorJuego.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.util.ArrayList;
+import model.Personaje;
+import model.interfaces.Curable;
+
+public class GestorJuego {
+
+    private ArrayList<Personaje> personajes = new ArrayList<>();
+
+}
