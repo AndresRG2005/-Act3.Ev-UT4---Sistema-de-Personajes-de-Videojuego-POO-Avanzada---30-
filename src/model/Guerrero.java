b696d7c2b15abd5f169a8499d65b3de018ab347e
Andrés <andresramosarinaga216@gmail.com>
2026-03-05 16:34:27 +0000

    Añado el constructor de la clase Guerrero

--- src/model/Guerrero.java
+++ src/model/Guerrero.java
@@ -8,4 +8,9 @@ public class Guerrero extends PersonajeFisico implements Defendible {
     private int fuerza;
     private int armadura;
 
+        public Guerrero(String nombre, int nivel, int salud, int fuerza, int armadura) {
+        super(nombre, nivel, salud);
+        this.fuerza = fuerza;
+        this.armadura = armadura;
+    }
 }
