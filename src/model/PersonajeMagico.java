23487aba22b33bc75997170578a82f7112ce0011
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 16:21:33 +0000

    Añadi el constructor de PersonajeMagico con el atributo mana

--- src/model/PersonajeMagico.java
+++ src/model/PersonajeMagico.java
@@ -2,5 +2,9 @@ package model;
 
 public abstract class PersonajeMagico extends Personaje {
     protected int mana;
-    
+
+    public PersonajeMagico(String nombre, int nivel, int salud, int mana) {
+        super(nombre, nivel, salud);
+        this.mana = mana;
+    }
 }
