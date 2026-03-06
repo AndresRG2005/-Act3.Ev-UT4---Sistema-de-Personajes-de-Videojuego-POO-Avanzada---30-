64016db6ed1392d4dd496813d3c0cbad6b0cf2e2
Daniel Dos Santos <d.santos2689@gmil.com>
2026-03-06 21:45:33 +0000

    Personajes Guerrero, Arquero, Mago y Clerigo en Main

--- src/app/main.java
+++ src/app/main.java
@@ -5,6 +5,15 @@ import model.*;
 public class main {
     public static void main(String[] args) {
         GestorJuego gestor = new GestorJuego();
+
+        Personaje g = new Guerrero("Andres" , 5, 100, 20, 10);
+        Personaje a = new Arquero("Aithamy", 4, 90, 15, 18);
+        Personaje m = new Mago("Daniel", 6, 80, 50, 25, 10);
+        Personaje c = new Clerigo("Claudia", 5, 85, 50, 12, 20);
+        
+
+
+
         
     }
 }
