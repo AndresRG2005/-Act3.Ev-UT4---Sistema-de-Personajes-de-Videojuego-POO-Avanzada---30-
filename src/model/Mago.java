e910a0f11ecd1658c85b8594171127e1c8da782a
Daniel Dos Santos <d.santos2689@gmil.com>
2026-03-06 22:24:55 +0000

    Implementar ataque magico basado en poderMagico

--- src/model/Mago.java
+++ src/model/Mago.java
@@ -4,4 +4,9 @@ import model.interfaces.Curable;
 public class Mago extends PersonajeMagico implements Curable {
     private int poderMagico;
     private int sabiduria;
+    public Mago(String nombre, int nivel, int salud, int poderMagico, int sabiduria, int mana) {
+        super(nombre, nivel, salud, mana);
+        this.poderMagico = poderMagico;
+        this.sabiduria = sabiduria;
+    }       
 }
