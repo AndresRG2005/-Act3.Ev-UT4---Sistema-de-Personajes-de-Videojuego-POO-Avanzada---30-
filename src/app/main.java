d7e95ec34b2aa45ceaa29f741d8015d1ab0d0c03
Daniel Dos Santos <d.santos2689@gmil.com>
2026-03-06 21:47:35 +0000

    Añadir personajes al GestorJuego desde Main

--- src/app/main.java
+++ src/app/main.java
@@ -10,7 +10,12 @@ public class main {
         Personaje a = new Arquero("Aithamy", 4, 90, 15, 18);
         Personaje m = new Mago("Daniel", 6, 80, 50, 25, 10);
         Personaje c = new Clerigo("Claudia", 5, 85, 50, 12, 20);
-        
+
+        juego.agregarPersonaje(g);
+        juego.agregarPersonaje(a);
+        juego.agregarPersonaje(m);
+        juego.agregarPersonaje(c);
+
 
 
 
