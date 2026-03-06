dbfe7bb6ad17487295feb457e857ef3890471bac
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 16:01:22 +0000

    Añadí el constructor a la clase Clerigo y cree sus atributos

--- src/model/Clerigo.java
+++ src/model/Clerigo.java
@@ -5,4 +5,10 @@ import model.interfaces.Curable;
 public class Clerigo extends PersonajeMagico implements Curable {
     private int fe;
     private int proteccion;
+
+    public Clerigo(String nombre, int nivel, int salud, int mana, int fe, int proteccion) {
+        super(nombre, nivel, salud, mana);
+        this.fe = fe;
+        this.proteccion = proteccion;
+    }
 }
