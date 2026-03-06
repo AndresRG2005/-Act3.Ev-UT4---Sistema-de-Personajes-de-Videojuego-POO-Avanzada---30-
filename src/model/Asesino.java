ac150f1f534901e6a299d93ae2b96c35f1821c8b
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 16:14:09 +0000

    Le añadi el constructor del Asesino con sus atributos

--- src/model/Asesino.java
+++ src/model/Asesino.java
@@ -3,4 +3,10 @@ package model;
 public class Asesino extends PersonajeFisico {
     private int sigilo;
     private int critico;
+
+    public Asesino(String nombre, int nivel, int salud, int sigilo, int critico) {
+        super(nombre, nivel, salud);
+        this.sigilo = sigilo;
+        this.critico = critico;
+    }
 }
