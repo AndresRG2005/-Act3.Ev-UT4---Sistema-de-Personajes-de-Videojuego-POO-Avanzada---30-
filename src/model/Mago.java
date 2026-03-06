6b3ece117f9d2e37b81993039bb5ba57bae35090
Daniel Dos Santos <d.santos2689@gmil.com>
2026-03-06 22:38:17 +0000

    Implementando metodos atacar basado en poderMagico y curar usando sabiduria

--- src/model/Mago.java
+++ src/model/Mago.java
@@ -8,5 +8,17 @@ public class Mago extends PersonajeMagico implements Curable {
         super(nombre, nivel, salud, mana);
         this.poderMagico = poderMagico;
         this.sabiduria = sabiduria;
-    }       
+
+        @Override
+        public void atacar(Personaje objetivo) {
+            int daño = poderMagico * 2;
+            objetivo.setSalud(objetivo.getSalud() - daño);
+            System.out.println(nombre + " lanza un hechizo a " + objetivo.getNombre() + " causando " + daño);
+        }    
+        
+        @Override
+        public void curar(Personaje objetivo) {
+            objetivo.setSalud(objetivo.getSalud() + sabiduria);
+            System.out.println(nombre + " cura a " + objetivo.getNombre());
+        }
 }
