f0010ccf1875d970804199e2b0c5586a753a0435
Daniel Dos Santos <d.santos2689@gmil.com>
2026-03-06 21:33:46 +0000

    Crear instancia de GestorJuego en Main

--- src/app/main.java
+++ src/app/main.java
@@ -2,9 +2,9 @@ package app;
 import controller.GestorJuego;
 import model.*;
 
-
-
 public class main {
     public static void main(String[] args) {
+        GestorJuego gestor = new GestorJuego();
+        
     }
 }
